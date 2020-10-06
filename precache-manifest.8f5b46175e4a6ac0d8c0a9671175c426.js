4e33b191f0209279c7352de785bf1ed7b7d49ff2
drsnails <anistu@gmail.com>
2020-10-06 15:02:30 +0300

    Updates

---
+++ precache-manifest.8f5b46175e4a6ac0d8c0a9671175c426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4246877719a434eee9214b4dd4d103",
+    "url": "/%E2%80%8Bhttps://drsnails.github.io/pixperfect%E2%80%8B/index.html"
+  },
+  {
+    "revision": "1d0aeafe7907e46a0519",
+    "url": "/%E2%80%8Bhttps://drsnails.github.io/pixperfect%E2%80%8B/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8c46b15fcdea0f90b7c",
+    "url": "/%E2%80%8Bhttps://drsnails.github.io/pixperfect%E2%80%8B/static/js/2.24915364.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%E2%80%8Bhttps://drsnails.github.io/pixperfect%E2%80%8B/static/js/2.24915364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0aeafe7907e46a0519",
+    "url": "/%E2%80%8Bhttps://drsnails.github.io/pixperfect%E2%80%8B/static/js/main.5be6d88e.chunk.js"
+  },
+  {
+    "revision": "87bfa7bdc2121584dd41",
+    "url": "/%E2%80%8Bhttps://drsnails.github.io/pixperfect%E2%80%8B/static/js/runtime-main.6ad01ed1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%E2%80%8Bhttps://drsnails.github.io/pixperfect%E2%80%8B/static/media/logo.5d5d9eef.svg"
+  }
+]);
